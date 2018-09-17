18eb0aa57891f55fdd089ba102457e1896625404
oussama-zaoui <karakoradoffy@gmail.com>
2018-09-17 22:30:13 +0100

    webView

---
+++ app/src/main/java/com/example/ouss/browser/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ouss.browser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.web);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.loadUrl("https://www.google.co.in");
+    }
+
+}
